32e5108b22e31eba5ba1ff99383988c6e5119839
kmwalton <ken.walton@gmail.com>
2021-07-28 14:48:01 -0400

    Create hgs-nnnn-to-en.py
    Convert a .nnnn-suffix file to a .Xen file so it may be used as a restart.
    
    e.g. Convert an arbitrary concentration file to a concentration restart file:
        hgs-nnnn-to-en.py prefixo.conc_pm.species.0042 prefixo.restart.cen

---
+++ bin/hgs-nnnn-to-en.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Convert a .nnnn-suffix file to a .Xen file so it may be used as a restart.
+
+e.g. Convert an arbitrary concentration file to a concentration restart file:
+    hgs-nnnn-to-en.py prefixo.conc_pm.species.0042 prefixo.restart.cen
+"""
+
+import sys
+import argparse
+
+argp = argparse.ArgumentParser()
+argp.add_argument('FILE_IN',
+    help='Input file',
+    )
+argp.add_argument('FILE_OUT',
+    help='Output file',
+    )
+args = argp.parse_args()
+
+with open(args.FILE_OUT, 'wb') as fout:
+    with open(args.FILE_IN, 'rb') as fin:
+        fin.seek(89)
+        fout.write(fin.read())
+
+sys.exit(0)
