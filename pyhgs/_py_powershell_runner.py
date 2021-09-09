afc9c98625ac754434485244088726c0d40c5516
kmwalton <ken.walton@gmail.com>
2021-09-09 17:12:54 -0400

    Enhancements to `eraseRunOutputs`
    Guard against removing files outside of the simulation directory.
    
    Look for a "keep_file" with a list of additional files to keep. This file may be a list of file names and/or globs.

--- pyhgs/_py_powershell_runner.py
+++ pyhgs/_py_powershell_runner.py
@@ -1,5 +1,11 @@
+"""HGS Runner for Window Powershell. PROVISIONAL
+
+This implementation will be removed when the pyhgsrunner is operational
+"""
 import sys
 import os
+import re
+import glob
 import subprocess
 import shutil
 from tempfile import SpooledTemporaryFile
@@ -16,30 +22,57 @@ class PyPowerShellRunner:                                              #{{{
         self.args = args
         self.simdir = simdir
         self.timeout = timeout
-        self.preexistingfiles = set(os.listdir(simdir))
+        self.captureRunPreexisting()
 
+    def captureRunPreexisting(self):
+        self.preexistingfiles = set(os.listdir(self.simdir))
 
-    def eraseRunOutputs(self, keep=[]):
+    def eraseRunOutputs(self, keep=[], keep_file=None):
         """Erase simulation output files.
 
-        This erases any files in the simulation directory that did not exist
-        prior to the creation of this object.
+        This erases any files *in the simulation directory* or subdirectory that
+        did not exist prior to the creation of this object or at the last call
+        to `captureRunPreexisting()`.
 
         Arguments:
             keep : list-like
                 A list of file names to keep; these will not be deleted, even if
                 they did not exist when this object was instantiated.
 
+            keep_file : str
+                Name of a file that lists filenames or glob-style expressions
+                (on separate lines) of additional files to keep, or None (or a
+                non-readable filename) if no keep files are to be
+                specified in this manner. 
         """
-        methodInvocationDir = os.getcwd()
-        os.chdir(self.simdir)
-        for f in set(os.listdir()) - self.preexistingfiles - set(keep):
-            if os.path.isdir(f):
-                shutil.rmtree(f)
-            else:
-                os.remove(f)
+        keep = set(keep)
+        if keep_file and os.path.isfile(keep_file):
+            with open(keep_file,'r') as fin:
+                for l in fin.readlines():
+                    l = l.strip()
+                    # check if the filename actually looks like a glob
+                    if re.search(r'[*[?]',l):
+                        keep.update( fn[len(self.simdir):]
+                                for fn in glob.iglob(os.path.join(simdir,l)))
+                    else:
+                        keep.update(l)
 
-        os.chdir(methodInvocationDir)
+        keep.update(self.preexistingfiles)
+
+        # scan for items in the keep list that have a directory component.
+        keep.update( d.split(os.sep)[0] for d in keep if os.path.dirname(d) )
+
+        fullsimdir = os.path.abspath(simdir)
+
+        for f in sorted(set(os.listdir(simdir)) - keep):
+            fullf = os.path.abspath(os.path.join(self.simdir,f))
+            if os.path.commonprefix(fullf,fullsimdir) != fullsimdir:
+                # file/directory not rooted below fsimdir
+                pass
+            elif os.path.isdir(ff):
+                shutil.rmtree(ff)
+            elif os.path.isfile(ff):
+                os.remove(ff)
     
 
     def runSim(self):
