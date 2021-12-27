d8c0cdae7ab67de5d35d20995368506cd14f8c8a
Ken Walton <ken.walton@gmail.com>
2021-12-27 08:08:41 -0500

    Updates/fixes to test case

--- test/sims/04b_coarse_refined_at_fx/preprocess-make-mesh.py
+++ test/sims/04b_coarse_refined_at_fx/preprocess-make-mesh.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
+import os
+import shutil
 import subprocess
 
 if __name__ == '__main__':
+
+    child_env = os.environ.copy()
    
     cp = subprocess.run(
-        [ 'ofrac2hgs.py',
+        [ 'python.exe', shutil.which('ofrac2hgs.py'),
             '--grid-out', 'gridinclude.grok',
             '--frac-out', 'fracinclude.grok',
             '--refine-near-fx-plane', '0.05', '0.10',
@@ -13,5 +17,6 @@ if __name__ == '__main__':
             '--', # end the list; begin positional arguments
             'fracs.rfd',
         ],
+        env=child_env,
         )
     cp.check_returncode()
