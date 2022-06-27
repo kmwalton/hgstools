b2eedc70b67a1e1e9a823804b307b94bc30ff129
kmwalton <ken.walton@gmail.com>
2022-06-27 15:16:45 -0400

    Fix: pathing of test case's files

--- test/test_parser/test_grok_parser.py
+++ test/test_parser/test_grok_parser.py
@@ -2,6 +2,7 @@
 """A unit test for pyghs.parser.grok"""
 
 import unittest
+import os
 
 import pyhgs.parser.grok
 
@@ -9,7 +10,9 @@ import pyhgs.parser.grok
 class Test_Solute(unittest.TestCase):
 
     def test_solute_props(self):
-        grok = pyhgs.parser.grok.parse('grok_parser_1/iel.grok')
+        grok = pyhgs.parser.grok.parse(
+                os.path.dirname(os.path.abspath(__file__))+os.path.sep+
+                'grok_parser_1/iel.grok')
         self.assertEqual(len(grok['solute']),1)
         self.assertTrue(abs(grok['solute']['TCE']['koc']-0.094) < 0.001)
 
