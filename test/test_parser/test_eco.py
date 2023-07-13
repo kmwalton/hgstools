8cd6f9c2793422d9a3c7172b52a9a8923cddae22
kmwalton <56695898+kmwalton@users.noreply.github.com>
2023-07-13 16:25:53 -0400

    Refactor for more consistent class/module names
    HGSEcoFileParser became EcoFile
    Deprecation warnings for the old class are in place, but it still functions identically to the previous code.

--- test/test_parser/test_eco.py
+++ test/test_parser/test_eco.py
@@ -4,23 +4,23 @@
 import unittest
 
 import os
-from pyhgs.parser.hgseco import HGSEcoFileParser
+from pyhgs.parser.eco import EcoFile
 
 DATDIR=os.path.abspath(os.path.dirname(__file__)+'/..')+os.path.sep
 
 class Test_ECO_Parser(unittest.TestCase):
 
     def test_ext_detect(self):
-        p = HGSEcoFileParser(DATDIR+'goodo.eco')
+        p = EcoFile(DATDIR+'goodo.eco')
         self.assertEqual(p.get_n_zones(), 1)
-        p = HGSEcoFileParser(DATDIR+'good')
+        p = EcoFile(DATDIR+'good')
         self.assertEqual(p.get_n_zones(), 1)
 
     def test_n_zone_reads(self):
-        p = HGSEcoFileParser(DATDIR+'goodo.eco')
+        p = EcoFile(DATDIR+'goodo.eco')
         self.assertEqual(p.get_n_zones(), 1)
 
-        p = HGSEcoFileParser(DATDIR+os.path.join(
+        p = EcoFile(DATDIR+os.path.join(
             'test_sims', 'undulating_coarse', 'undf'))
 
         self.assertEqual(p.get_n_zones(), 2)
@@ -28,11 +28,11 @@ class Test_ECO_Parser(unittest.TestCase):
 
     def test_zone_prop_reads(self):
         with self.subTest('goodo.eco'):
-            p = HGSEcoFileParser(DATDIR+'goodo.eco')
+            p = EcoFile(DATDIR+'goodo.eco')
             self.assertEqual(p.get_pm_zone_properties(), [(1,'sandstone',),])
 
         with self.subTest('undfo.eco'):
-          p = HGSEcoFileParser(DATDIR+os.path.join(
+          p = EcoFile(DATDIR+os.path.join(
             'test_sims', 'undulating_coarse', 'undf',))
 
           self.assertEqual(
@@ -41,7 +41,7 @@ class Test_ECO_Parser(unittest.TestCase):
              (2,'porous medium 2',),])
 
     def test_get_output_times(self):
-        p = HGSEcoFileParser(DATDIR+'goodo.eco')
+        p = EcoFile(DATDIR+'goodo.eco')
         self.assertEqual(len(p.get_output_times()), 88)
 
 
